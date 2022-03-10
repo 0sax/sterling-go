6bdfefc4bcc280cabd232f72d0ab460058413bc4
Lord-ofada <omfaluyi@gmail.com>
2022-03-10 13:50:22 +0100

    Added two more error cases

--- presenters.go
+++ presenters.go
@@ -14,6 +14,40 @@ const (
 	interBankFT          = 101
 )
 
+var errorCodes = map[string]string{
+	"00": "Approved or completed successfully",
+	"03": "Invalid Sender",
+	"05": "Do not honor",
+	"06": "Dormant Account",
+	"07": "Invalid Account",
+	"08": "Account Name Mismatch",
+	"09": "Request processing in progress",
+	"12": "Invalid transaction",
+	"13": "Invalid Amount",
+	"14": "Invalid Batch Number",
+	"15": "Invalid Session or Record ID",
+	"16": "Unknown Bank Code",
+	"17": "Invalid Channel",
+	"18": "Wrong Method Call",
+	"21": "No action taken",
+	"25": "Unable to locate record",
+	"26": "Duplicate record",
+	"30": "Format error",
+	"34": "Suspected fraud",
+	"35": "Contact sending bank",
+	"51": "No sufficient funds",
+	"57": "Transaction not permitted to sender",
+	"58": "Transaction not permitted on channel",
+	"61": "Transfer limit Exceeded",
+	"63": "Security violation",
+	"65": "Exceeds withdrawal frequency",
+	"68": "Response received too late",
+	"91": "Beneficiary Bank not available",
+	"92": "Routing error",
+	"94": "Duplicate transaction",
+	"96": "System malfunction",
+}
+
 // Requests
 type (
 	IBSRequest struct {
